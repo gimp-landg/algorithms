bbce3f7456adf2c1b2e258304ce177855ea50077
Yannis Gkoumas <ygkoumas@gmail.com>
2017-04-20 00:38:16 +0100

    Optimize common-child challenge

--- common-child/__main__.py
+++ common-child/__main__.py
@@ -2,19 +2,38 @@
 # https://www.hackerrank.com/challenges/common-child
 
 def get_longest_common_child_length(a_string, b_string):
-	a_preproc = [c for c in a_string if c in b_string]
-	b_preproc = [c for c in b_string if c in a_string]
+	a_set = set(a_string)
+	b_set = set(b_string)
+	a_preproc = [c for c in a_string if c in b_set]
+	b_preproc = [c for c in b_string if c in a_set]
 
-	a_children = find_all_children(a_preproc)
-	b_children = find_all_children(b_preproc)
+	common_children = find_all_children(a_preproc, b_preproc)
 
-	return max([len(w) for w in a_children & b_children])
+	return max([len(w) for w in common_children])
 
-def find_all_children(string):
-	string_children =['']
-	for c in string:
-		string_children += [w+c for w in string_children]
-	return set(string_children)
+# input of find_all_children can be list as well as string
+def find_all_children(a_string, b_string):
+	children = set([''])
+	for c in a_string:
+		map(children.add, [w+c for w in children if is_child_of(w+c,b_string)])
+	return children
+
+def is_child_of(child, parent):
+	child_len = len(child)
+	if child_len <= len(parent):
+		i=0
+		cc = child[i]
+		for pc in parent:
+			if pc == cc:
+				if i == child_len-1:
+					return True
+				else:
+					i += 1
+					cc = child[i]
+		return False
+	else:
+		return False
+
 
 if __name__ == '__main__':
 	a_string = raw_input().strip()
