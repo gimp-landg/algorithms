2dbae5329b62c0e944fa0aeca837efa5d6583e81
Yannis Gkoumas <ygkoumas@gmail.com>
2017-04-24 20:41:23 +0100

    Avoid recursiveness on Common-child challenge

--- common-child/__main__.py
+++ common-child/__main__.py
@@ -15,24 +15,18 @@ def _init_longest_common_child_length_map(a_string, b_string):
 	global longest_common_child_length_map
 	a_range = range(len(a_string)+1)
 	b_range = range(len(b_string)+1)
-	longest_common_child_length_map = {(a_string[0:i], b_string[0:j]) : None for i in a_range for j in b_range}
-	for ab in longest_common_child_length_map:
-		if ab[0] == '' or ab[1] == '':
-			longest_common_child_length_map[ab]=0
-
+	longest_common_child_length_map = {(a_string[0:i], b_string[0:j]) : 0 for i in a_range for j in b_range}
 
 def _get_longest_common_child_length(a_string, b_string):
 	global longest_common_child_length_map
 	lcclm = longest_common_child_length_map
-
-	if lcclm[(a_string, b_string)] is not None:
-		return lcclm[(a_string, b_string)]
-
-	if a_string[-1] != b_string[-1]:
-		lcclm[(a_string, b_string)] = max(_get_longest_common_child_length(a_string[0:-1], b_string),
-			_get_longest_common_child_length(a_string, b_string[0:-1]))
-	else:
-		lcclm[(a_string, b_string)] = _get_longest_common_child_length(a_string[0:-1], b_string[0:-1]) + 1
+	for i in range(1, len(a_string)+1):
+		for j in range(1, len(b_string)+1):
+			if a_string[i-1] != b_string[j-1]:
+				lcclm[(a_string[0:i], b_string[0:j])] = max(lcclm[(a_string[0:i-1], b_string[0:j])],
+					lcclm[(a_string[0:i], b_string[0:j-1])])
+			else:
+				lcclm[(a_string[0:i], b_string[0:j])] = lcclm[(a_string[0:i-1], b_string[0:j-1])] + 1
 	return lcclm[(a_string, b_string)]
 
 
