6cbe1b0397e293c8240b8feebc875073bc0e8359
Yannis Gkoumas <ygkoumas@gmail.com>
2017-04-24 19:33:13 +0100

    Use dynamic programming and recursiveness for common-child challenge
    Use dynamic programming:
    Divide the problem into smaller problems of substring of the target strings
    Use recursiveness:
    Store data that have already been calculated for reuse

--- common-child/__main__.py
+++ common-child/__main__.py
@@ -1,38 +1,39 @@
-# common-child challenge
+# common-child challenge from
 # https://www.hackerrank.com/challenges/common-child
 
 def get_longest_common_child_length(a_string, b_string):
 	a_set = set(a_string)
 	b_set = set(b_string)
-	a_preproc = [c for c in a_string if c in b_set]
-	b_preproc = [c for c in b_string if c in a_set]
+	a_preproc = ''.join([c for c in a_string if c in b_set])
+	b_preproc = ''.join([c for c in b_string if c in a_set])
 
-	common_children = find_all_children(a_preproc, b_preproc)
+	_init_longest_common_child_length_map(a_preproc, b_preproc)
 
-	return max([len(w) for w in common_children])
+	return _get_longest_common_child_length(a_preproc, b_preproc)
 
-# input of find_all_children can be list as well as string
-def find_all_children(a_string, b_string):
-	children = set([''])
-	for c in a_string:
-		map(children.add, [w+c for w in children if is_child_of(w+c,b_string)])
-	return children
+def _init_longest_common_child_length_map(a_string, b_string):
+	global longest_common_child_length_map
+	a_range = range(len(a_string)+1)
+	b_range = range(len(b_string)+1)
+	longest_common_child_length_map = {(a_string[0:i], b_string[0:j]) : None for i in a_range for j in b_range}
+	for ab in longest_common_child_length_map:
+		if ab[0] == '' or ab[1] == '':
+			longest_common_child_length_map[ab]=0
 
-def is_child_of(child, parent):
-	child_len = len(child)
-	if child_len <= len(parent):
-		i=0
-		cc = child[i]
-		for pc in parent:
-			if pc == cc:
-				if i == child_len-1:
-					return True
-				else:
-					i += 1
-					cc = child[i]
-		return False
+
+def _get_longest_common_child_length(a_string, b_string):
+	global longest_common_child_length_map
+	lcclm = longest_common_child_length_map
+
+	if lcclm[(a_string, b_string)] is not None:
+		return lcclm[(a_string, b_string)]
+
+	if a_string[-1] != b_string[-1]:
+		lcclm[(a_string, b_string)] = max(_get_longest_common_child_length(a_string[0:-1], b_string),
+			_get_longest_common_child_length(a_string, b_string[0:-1]))
 	else:
-		return False
+		lcclm[(a_string, b_string)] = _get_longest_common_child_length(a_string[0:-1], b_string[0:-1]) + 1
+	return lcclm[(a_string, b_string)]
 
 
 if __name__ == '__main__':
