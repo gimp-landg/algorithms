ae95dd072153e1a78c911215e488e0a743566a66
Yannis Gkoumas <ygkoumas@gmail.com>
2017-04-26 22:34:53 +0100

    Minor variable changes on common-child challenge

--- common-child/__main__.py
+++ common-child/__main__.py
@@ -10,17 +10,17 @@ def get_longest_common_child_length(a_string, b_string):
 	return _get_longest_common_child_length(a_preproc, b_preproc)
 
 def _get_longest_common_child_length(a_string, b_string):
-	a_len = len(a_string)
-	b_len = len(b_string)
+	a_len = len(a_string) + 1
+	b_len = len(b_string) + 1
 	# longest common child length table as lcclt
-	lcclt = [[0]*(b_len+1) for i in range(a_len+1)]
-	for i in range(1, a_len+1):
-		for j in range(1, b_len+1):
+	lcclt = [[0]*(b_len) for i in range(a_len)]
+	for i in range(1, a_len):
+		for j in range(1, b_len):
 			if a_string[i-1] != b_string[j-1]:
 				lcclt[i][j] = max(lcclt[i-1][j], lcclt[i][j-1])
 			else:
 				lcclt[i][j] = lcclt[i-1][j-1] + 1
-	return lcclt[a_len][b_len]
+	return lcclt[-1][-1]
 
 
 if __name__ == '__main__':
