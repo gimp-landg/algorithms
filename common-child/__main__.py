572af0e5f64c29bf050e90131e10585ba462f157
Yannis Gkoumas <ygkoumas@gmail.com>
2017-04-27 00:08:43 +0100

    Use enumerate on common-child challenge

--- common-child/__main__.py
+++ common-child/__main__.py
@@ -14,12 +14,13 @@ def _get_longest_common_child_length(a_string, b_string):
 	b_len = len(b_string) + 1
 	# longest common child length table as lcclt
 	lcclt = [[0]*(b_len) for i in range(a_len)]
-	for i in range(1, a_len):
-		for j in range(1, b_len):
-			if a_string[i-1] != b_string[j-1]:
-				lcclt[i][j] = max(lcclt[i-1][j], lcclt[i][j-1])
+
+	for i, x in enumerate(a_string):
+		for j, y in enumerate(b_string):
+			if x != y:
+				lcclt[i+1][j+1] = max(lcclt[i][j+1], lcclt[i+1][j])
 			else:
-				lcclt[i][j] = lcclt[i-1][j-1] + 1
+				lcclt[i+1][j+1] = lcclt[i][j] + 1
 	return lcclt[-1][-1]
 
 
