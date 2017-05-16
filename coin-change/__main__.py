32b1d1bcc0529f415574bd622a824b97cae59f6b
Yannis Gkoumas <ygkoumas@gmail.com>
2017-05-16 21:02:18 +0100

    Rename global variables on coin-change

--- coin-change/__main__.py
+++ coin-change/__main__.py
@@ -1,26 +1,26 @@
 # submitted to hackerrank
 # https://www.hackerrank.com/challenges/coin-change
-def init(nl, Cl):
-	global ways_table, n, C
-	n = nl
-	C = Cl
-	ways_table = [{i:0 for i in C} for j in range(n+1)]
-	for wt in ways_table: wt.update({'calculated' : False})
+def init(n, c):
+	global WAYS_TABLE, N, C
+	N = n
+	C = c
+	WAYS_TABLE = [{i:0 for i in C} for j in range(N+1)]
+	for wt in WAYS_TABLE: wt.update({'calculated' : False})
 
 def get_ways(i):
-	if not ways_table[i]['calculated']:
+	if not WAYS_TABLE[i]['calculated']:
 		for j in C:
 			if j < i:
-				ways_table[i][j] = sum([get_ways(i-j)[k] for k in C if k<=j])
+				WAYS_TABLE[i][j] = sum([get_ways(i-j)[k] for k in C if k<=j])
 		if i in C:
-			ways_table[i][i] = 1
-		ways_table[i]['calculated'] = True
-	return ways_table[i]
+			WAYS_TABLE[i][i] = 1
+		WAYS_TABLE[i]['calculated'] = True
+	return WAYS_TABLE[i]
 
 if __name__ == '__main__':
-	n, m = raw_input().strip().split(' ')
-	n, m = [int(n), int(m)]
+	N, M = raw_input().strip().split(' ')
+	N, M = [int(N), int(N)]
 	C = map(int, raw_input().strip().split(' '))
 
-	init(n, C)
-	print sum([get_ways(n)[k] for k in C])
+	init(N, C)
+	print sum([get_ways(N)[k] for k in C])
